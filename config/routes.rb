167ca0c01b8d34295a65ceec765d30319ec2d1fb
TrangHo <carepjan@KuroiTamago.local>
2017-03-29 20:14:09 -0500

    Change to use slim instead of erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  
+
   root 'pages#home'
-  
-  get 'pages/about'
-
-  get 'pages/home'
-
-  get 'pages/problems'
+  get :home, controller: :pages, action: :home
+  get :about, controller: :pages, action: :about
+  get :problems, controller: :pages, action: :problems
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
